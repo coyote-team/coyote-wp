2e6bd18edf9b3fa741e4609a21a3b56083f49aed
Job van Achterberg <mail@3ode.nl>
2020-04-16 12:35:18 +0000

    Add plugin table create/update/retrieve functions

--- php/classes/class.db.php
+++ php/classes/class.db.php
@@ -4,7 +4,7 @@ namespace Coyote;
 
 class DB {
 
-    public function associate_resources_with_post(array $resource, WP_Post $post) {
+    public static function associate_resources_with_post(array $resource, WP_Post $post) {
         global $wpdb;
 
         foreach ($resources as $resource) {
@@ -12,49 +12,14 @@ class DB {
         }
     }
 
-    public function get_image_by_hash($hash) {
-    }
-
-    public function insert_image($hash, $src, $alt, $resourceId) {
-    }
-
-    public function batch_fetch_or_create_entry($images) {
-        $entries = [];
-
-        foreach ($images as $image) {
-            $hash = sha1($image["src"]);
-
-            $entry = self::_fetch_entry($hash);
-
-            if ($entry === null) {
-                $entry = $self::_create_entry($hash, $image);
-            }
-
-            array_push($entries, $entry);
-        }
-
-        return $entries;
-    }
-
-    private function _fetch_entry($hash) {
-        global $wpdb;
-        
-        $prepared_query = $wpdb->prepare(
-            "SELECT * FROM %s WHERE source_uri_sha1 = %s",
-            COYOTE_IMAGE_TABLE_NAME, $hash
-        );
-
-        $wpdb->get_row($prepared_query);
-    }
-
-    private function _create_entry($hash, $image) {
+    public static function insert_image($hash, $src, $alt, $resourceId) {
         global $wpdb;
 
         $record = [
             $hash,
-            $image["src"],
-            null,
-            $image["alt"],
+            $src,
+            (int) $resourceId,
+            $alt,
             ""
         ];
 
@@ -62,6 +27,20 @@ class DB {
 
         $wpdb->insert(COYOTE_IMAGE_TABLE_NAME, $record, $data_types);
 
-        return self::_fetch_entry($hash);
+        return self::get_image_by_hash($hash);
+    }
+
+    public static function get_image_by_hash($hash) {
+        global $wpdb;
+        
+        $prepared_query = $wpdb->prepare(
+            "SELECT * FROM " . COYOTE_IMAGE_TABLE_NAME . " WHERE source_uri_sha1 = %s",
+            $hash
+        );
+
+        $wpdb->get_row($prepared_query);
+    }
+
+    private function _create_entry($hash, $image) {
     }
 }
