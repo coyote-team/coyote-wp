cf4b4b39cabcea42e2adb90ddf5b2ba7a866e3fa
Job van Achterberg <mail@3ode.nl>
2020-04-24 12:15:36 +0000

    Add primary key constraint for upserting

--- sql/create_join_table.sql
+++ sql/create_join_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE %resource_post_join_table_name% (
     coyote_resource_id BIGINT(20) UNSIGNED NOT NULL,
     wp_post_id BIGINT(20) UNSIGNED NOT NULL,
     FOREIGN KEY (coyote_resource_id) REFERENCES %image_resource_table_name%(coyote_resource_id),
-    FOREIGN KEY (wp_post_id) REFERENCES %wp_post_table_name%(ID) ON DELETE CASCADE
+    FOREIGN KEY (wp_post_id) REFERENCES %wp_post_table_name%(ID) ON DELETE CASCADE,
+    PRIMARY KEY (coyote_resource_id, wp_post_id)
 ) %charset_collate%;
