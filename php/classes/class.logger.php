92625368a7b70227f7afaacd83f741bb114dbc3c
Job van Achterberg <mail@3ode.nl>
2020-05-04 08:02:50 +0000

    Always log to error log

--- php/classes/class.logger.php
+++ php/classes/class.logger.php
@@ -10,12 +10,8 @@ if (!defined( 'ABSPATH')) {
 class Logger {
 
     public static function log($log) {
-        if (true === WP_DEBUG) {
-            if (is_array($log) || is_object($log)) {
-                error_log(print_r($log, true));
-            } else {
-                error_log($log);
-            }
+        if (is_array($log) || is_object($log)) {
+            error_log(print_r($log, true));
         }
     }
 
