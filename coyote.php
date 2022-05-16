5a3e72e5d5c6bd4658a6b1c963b743312306345d
Job van Achterberg <mail@3ode.nl>
2022-05-16 15:12:28 +0200

    Bump version to 2.0

--- coyote.php
+++ coyote.php
@@ -4,7 +4,7 @@ Plugin Name: Coyote
 Description: Integrate with a Coyote API to obtain media text descriptions.
 Plugin URI: http://wordpress.org/plugins/coyote/
 Author: Prime Access Consulting
-Version: 1.7
+Version: 2.0
 Author URI: https://www.pac.bz
  */
 
