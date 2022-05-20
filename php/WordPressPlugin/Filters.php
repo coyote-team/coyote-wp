e2b5ac6d413b09ea5707f04741319f20996455d6
Job van Achterberg <mail@3ode.nl>
2022-05-20 14:25:54 +0200

    Various stylistic changes

--- php/WordPressPlugin/Filters.php
+++ php/WordPressPlugin/Filters.php
@@ -7,31 +7,35 @@ use Coyote\Traits\Logger;
 use Coyote\WordPressHelper;
 use Coyote\WordPressImage;
 use Coyote\ContentHelper\Image;
+use WP_Post;
 
 class Filters
 {
     use Logger;
 
     // add setting quicklink to plugin listing entry
-    public static function addActionLinks($links): array {
+    public static function addActionLinks($links): array
+    {
         $url = admin_url('options-general.php?page=coyote_fields');
         $text = __('Settings');
 
         $settings_links = [
-            "<a href=\"{$url}\">{$text}</a>"
+            "<a href=\"$url\">$text</a>"
         ];
 
         return array_merge($links, $settings_links);
     }
 
-    public static function addTinyMcePlugin() {
-        add_filter('mce_external_plugins', function($plugins) {
+    public static function addTinyMcePlugin()
+    {
+        add_filter('mce_external_plugins', function ($plugins) {
             $plugins['coyote'] = coyote_asset_url('tinymce_plugin.js');
             return $plugins;
         });
     }
 
-    public static function filterPostContent($postContent) {
+    public static function filterPostContent($postContent)
+    {
         global $post;
 
         if ($post->post_type === 'attachment') {
@@ -43,7 +47,8 @@ class Filters
     }
 
     // used in the media template
-    public static function filterAttachmentForJavaScript($response, $attachment, $meta) {
+    public static function filterAttachmentForJavaScript($response, $attachment, $meta)
+    {
         if ($response['type'] !== 'image') {
             return $response;
         }
@@ -74,7 +79,8 @@ class Filters
         return $response;
     }
 
-    public static function filterAttachmentImageAttributes(array $attr, \WP_Post $attachment, int $size): array {
+    public static function filterAttachmentImageAttributes(array $attr, WP_Post $attachment, int $size): array
+    {
         // get a coyote resource for this attachment. If not found, try to create it unless
         // running in standalone mode.
 
@@ -93,7 +99,8 @@ class Filters
         return $attr;
     }
 
-    public static function addCronSchedule($schedules) {
+    public static function addCronSchedule($schedules)
+    {
         $schedules['five_minutes'] = [
             'interval' => 300,
             'display'  => esc_html__('Every Five Minutes')
