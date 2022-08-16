17d0131b4075196bda213af0b61700e4197a4dc3
Ruud Mulleman <ruud.mulleman@voormeerwaarde.nl>
2022-08-16 12:07:08 +0200

    Added translation files

--- php/WordPressPlugin/Actions.php
+++ php/WordPressPlugin/Actions.php
@@ -135,4 +135,21 @@ js;
     {
         self::logDebug('Deactivating plugin');
     }
+
+    /**
+     * Load the plugin text domain for translation.
+     */
+    public static function loadPluginTextdomain(): void
+    {
+        $test = load_plugin_textdomain(
+            WordPressPlugin::I18N_NS,
+            false,
+            COYOTE_TRANSLATION_PATH
+        );
+        if($test) {
+            echo 'true';
+        } else {
+            echo 'false';
+        }
+    }
 }
