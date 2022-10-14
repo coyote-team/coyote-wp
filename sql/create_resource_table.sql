b4c7635d45e0d7297aca778e9c520f9b9e02fc26
Job van Achterberg <mail@3ode.nl>
2022-10-14 12:47:38 +0200

    Remove default empty string for text columns in table definition as per spec

--- sql/create_resource_table.sql
+++ sql/create_resource_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE %image_resource_table_name% (
     source_uri_sha1 VARCHAR(40) NOT NULL,
     source_uri MEDIUMTEXT NOT NULL,
     coyote_resource_id BIGINT(20) UNSIGNED NOT NULL,
-    original_description TEXT DEFAULT '' NOT NULL,
-    coyote_description TEXT DEFAULT '' NOT NULL,
+    original_description TEXT NOT NULL,
+    coyote_description TEXT NOT NULL,
     PRIMARY KEY (source_uri_sha1),
     INDEX (coyote_resource_id)
 ) %charset_collate%;
