2c91806b62027e820c7bc9e51c0c0d1388211c78
Job van Achterberg <mail@3ode.nl>
2023-04-17 12:49:35 +0200

    Remove monolog dependency

--- php/Traits/Logger.php
+++ php/Traits/Logger.php
@@ -2,46 +2,50 @@
 
 namespace Coyote\Traits;
 
-use Coyote\WordPressPlugin;
-use PAC_Vendor\Monolog\Logger as MonologLogger;
-use PAC_Vendor\Monolog\Handler\StreamHandler;
-
 if (!defined('WPINC')) {
     exit;
 }
 
 trait Logger
 {
+    static int $LOG_DEBUG = 100;
+    static int $LOG_INFO = 200;
+    static int $LOG_WARNING = 300;
+    static int $LOG_ERROR = 400;
+
     public static function logInfo(string $message, $data = []): void
     {
-        self::log($message, $data, MonologLogger::INFO, get_called_class());
+        self::log($message, $data, self::$LOG_INFO, get_called_class());
     }
 
     public static function logWarning(string $message, $data = []): void
     {
-        self::log($message, $data, MonologLogger::WARNING, get_called_class());
+        self::log($message, $data, self::$LOG_WARNING, get_called_class());
     }
 
     public static function logDebug(string $message, $data = []): void
     {
-        self::log($message, $data, MonologLogger::DEBUG, get_called_class());
+        self::log($message, $data, self::$LOG_DEBUG, get_called_class());
     }
 
     public static function logError(string $message, $data = []): void
     {
-        self::log($message, $data, MonologLogger::ERROR, get_called_class());
+        self::log($message, $data, self::$LOG_ERROR, get_called_class());
     }
 
     private static function log(string $message, array $payload, int $level, string $class): void
     {
-        self::logger()->log($level, $message, array_merge($payload, ['class' => $class]));
-    }
+        $levels = [
+            self::$LOG_DEBUG => 'DEBUG',
+            self::$LOG_INFO => 'INFO',
+            self::$LOG_WARNING => 'WARNING',
+            self::$LOG_ERROR => 'ERROR'
+        ];
 
-    private static function logger(): MonologLogger
-    {
-        $logger = new MonologLogger(WordPressPlugin::PLUGIN_NAME);
-        $handler = new StreamHandler(WordPressPlugin::LOG_PATH, \Monolog\Logger::DEBUG);
-        $logger->pushHandler($handler);
-        return $logger;
+        if (defined('WP_DEBUG') && WP_DEBUG) {
+            $payload = print_r($payload, true);
+            $message = sprintf("[%s] %s: %s > %s", $levels[$level], $class, $message, $payload);
+            error_log($message);
+        }
     }
 }
